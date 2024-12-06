2b766f2dfdd504cb9338872961f09be59de54fe0
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-06 22:58:27 +0700

    Add files via upload
    Hoàn thanh baitap3_ss2

---
+++ baitap3_ss2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 20;
+	int b = 10;
+	
+	int sum = 0;
+	
+	sum = a + b;
+	int hieu = a - b;
+	int thuong = a / b;
+	int tich = a * b;
+	
+	printf("%d\n", sum);
+	printf("%d\n", hieu);
+	printf("%d\n", thuong);
+	printf("%d\n", tich);
+	
+	
+	return 0;
+}
