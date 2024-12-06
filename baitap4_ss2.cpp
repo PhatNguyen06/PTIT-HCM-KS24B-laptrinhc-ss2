34c2b6a1438778888c6ba98cac632db9ae9e1774
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-06 22:58:37 +0700

    Add files via upload
    Hoàn thanh baitap4_ss2

---
+++ baitap4_ss2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int canh_hinh_vuong = 6;
+	
+	int dien_tich_hv = canh_hinh_vuong * canh_hinh_vuong;
+	int chu_vi_hv = canh_hinh_vuong * 4;
+	
+	printf("%d\n", dien_tich_hv);
+	printf("%d\n", chu_vi_hv);
+	
+	return 0;
+}
