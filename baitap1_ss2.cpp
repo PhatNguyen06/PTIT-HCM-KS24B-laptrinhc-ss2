54d8c44c1eb0cd4cb47d84d34159184d618a5f02
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-06 22:57:56 +0700

    Add files via upload
    Hoàn thanh baitap1_ss2

---
+++ baitap1_ss2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int a; // kieu du lieu interger, luu so nguyen khoang -2,147,483,648 -> 2,147,483,647, co 4 byte
+	float b; // kieu du lieu float, luu so thuc 3.4E-38 -> 3.4E+38, 4 byte
+	long long c; // kieu du lieu long long, luu so nguyen giong int nhung kich thuoc hon hon, 0 -> 18446744073709551615 va co 8 byte
+	double d; // kieu du lieu double, luu so thuc giong float nhung kich thuoc lon hon, 1.7E-308 -> 1.7E+308 va co 8 byte
+	char e; // kieu du lieu char, luu so nguyen, ki tu, gioi han luu tru -128 -> 127, co kich thuoc 1 byte
+	
+	array // kieu du lieu dan xuat
+	pointer // kieu du lieu con tro
+	struct; // kieu du lieu phan nang cao
+	void // kieu du lieu ham khong tra ve gia tri nao het
+	
+	
+	return 0;
+}
